9a18d2f5cb1afde93e7b9c81c7f0d911de8def28
Genis Bardales <genis96@yahoo.com>
2020-12-02 20:40:43 -0500

    added questions to js

---
+++ JS/questions.js
@@ -0,0 +1,19 @@
+//Create the questions/multiple choices/answers & use an object array 
+let questions = [{
+        question: 'CSS stands for: ',
+        choice: ['Cascading Style Sheets', 'Carrot Styling Sheets', 'Cascading Setup Screen'],
+        answer: 'Cascading Style Sheets'
+    },
+    {
+        question: 'Which is NOT a boolean operator?',
+        choice: ['&&', '||', '!', '${}'],
+        answer: '${}'
+    },
+    {
+        question: 'Which of the following is a way to form a loop?',
+        choice: ['for loop', 'for in', 'forEach', 'while', 'all the above'],
+        answer: 'all the above'
+    }
+];
+
+
