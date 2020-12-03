9d32e102d252bb2e386c2d31775d687a82e5fc4a
Genis Bardales <genis96@yahoo.com>
2020-12-02 21:33:17 -0500

    working on JS in script file

--- JS/questions.js
+++ JS/questions.js
@@ -1,16 +1,16 @@
 //Create the questions/multiple choices/answers & use an object array 
-let questions = [{
-        question: 'CSS stands for: ',
+let question = [{
+        questions: 'CSS stands for: ',
         choice: ['Cascading Style Sheets', 'Carrot Styling Sheets', 'Cascading Setup Screen'],
         answer: 'Cascading Style Sheets'
     },
     {
-        question: 'Which is NOT a boolean operator?',
+        questions: 'Which is NOT a boolean operator?',
         choice: ['&&', '||', '!', '${}'],
         answer: '${}'
     },
     {
-        question: 'Which of the following is a way to form a loop?',
+        questions: 'Which of the following is a way to form a loop?',
         choice: ['for loop', 'for in', 'forEach', 'while', 'all the above'],
         answer: 'all the above'
     }
--- JS/script.js
+++ JS/script.js
@@ -5,4 +5,15 @@ let submitBtn = document.querySelector('.submit-btn');
 let questions = document.querySelector('#questions');
 let multipleChoice = document.querySelector('#multiple-choice');
 let nameStorage = document.querySelector('#name-storage');
+let finalScore = document.querySelector('.final-score');
+
+function startGame() {
+    let indexPage = document.querySelector('#index-page') = indexPage.setAttribute('class', 'hide');
+    console.log(indexPage);
+}
+
+startBtn.addEventListener('click', startGame);
+
+
+
 
