e1cfb80e94e408b6b166d65da3df61c815b94a72
Genis Bardales <genis96@yahoo.com>
2020-12-02 20:27:11 -0500

    started with the js and some other html edits

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+let score = document.querySelector('.score');
+let time = document.querySelector('.time');
+let startBtn = document.querySelector('.start-btn');
+let submitBtn = document.querySelector('.submit-btn');
+let questions = document.querySelector('#questions');
+let multipleChoice = document.querySelector('#multiple-choice');
+let nameStorage = document.querySelector('#name-storage');
+
